c36fcf669cf6a4660a3dd6749cb00c68688995d7
ShortyDev <shortydev8274@gmail.com>
2022-11-25 09:53:04 +0100

    changed ranking

--- src/components/Ranking.tsx
+++ src/components/Ranking.tsx
@@ -7,14 +7,14 @@ export const Ranking = () => {
     const getList = async () => {
         const response = await fetch("https://sortiermeister-backend.shorty8274.workers.dev/b4c0351f53/list")
         const data = await response.json()
-        setList(data[0].players.sort((a: any, b: any) => a.data.time - b.data.time).slice(0, 10))
+        setList(data[0].players.sort((a: any, b: any) => a.data.time - b.data.time))
     }
 
     useEffect(() => {
         getList();
     }, [])
     return (
-        <div className="flex-col flex mt-5">
+        <div className="flex-col flex mt-5 overflow-y-scroll max-h-96">
             <h1 className="text-4xl font-dosis text-primary">Ranking</h1>
             <div className="flex flex-col">
                 {list.map((item: any, index: number) =>
